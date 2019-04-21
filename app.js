694bd6d1789849451bbd47280a4cb2d9ccd72778
Mr-Maverick <sumit.mav@gmail.com>
2019-04-21 15:03:26 +0530

    multer

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ const http      = require('http').Server(app);
 const bodyParser = require('body-parser');
 const ejs       = require('ejs');
 const io        = require('socket.io')(8080);
-
+const multer    = require('multer')
+const path      = require('path')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public_static'))
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public_static/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+  })
+const upload    = multer({ storage : storage })
+
 app.get('/', (req,res) => {
     res.render('index.ejs')
-})
-
-app.get('/localVideo', (req,res) => {
-    res.render('localVideo.ejs')
 })
 
 app.get('/video', (req,res)=>{
@@ -28,6 +35,15 @@ app.get('/video', (req,res)=>{
 
 app.post('/video', (req,res)=>{
     redirectLink = '/video?url=' + req.body.videoLink
+    res.redirect(redirectLink)
+})
+
+app.get('/localVideo', (req,res) => {
+    res.render('localVideo.ejs', {url : req.query.url})
+})
+
+app.post('/localVideo', upload.single('local'), (req,res)=>{
+    redirectLink = '/localVideo?url=' + req.file.filename
     res.redirect(redirectLink)
 })
 
